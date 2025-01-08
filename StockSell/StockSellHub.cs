852d9a15281c492a90a4f0c9c1a541cfd0c1afd9
EduMarsola <eduimarsola@gmail.com>
2025-01-08 16:47:16 -0300

    08/01/2025

--- StockSell/StockSellHub.cs
+++ StockSell/StockSellHub.cs
@@ -14,55 +14,76 @@ namespace StockSell
 {
     public partial class StockSellHub : Form
     {
-        static string stringConnection = "server=localhost; port=3306; user id=root; password=0";
+        static string stringConnection = "server=localhost; database=StockSellMaster; port=3306; user id=root; password=0";
         private MySqlConnection conn = new MySqlConnection(stringConnection);
         //database StockSellMaster
-        public StockSellHub()
-        {
-            InitializeComponent();
-            conn.Open();
+        /*tabela cliente
+         * create table clientes(" +
+            "CPF varchar(11) not null unique," +
+            "NomeCompleto text," +
+            "DataNascimento date," +
+            "primary key(CPF)" +
+            ");
+        */
+
+        /*tabela Produtos
+           create table produtos(
+            idVenda integer autoincrement,
+            idProduto integer not null,
             
-        }
+            )
+         */
+public StockSellHub()
+{
+    InitializeComponent();
+    conn.Open();
 
-        private void buttonCliente_Click(object sender, EventArgs e)
-        {
-            Cliente cliente = new Cliente(this, conn);
-            cliente.Show();
-        }
+}
 
-        private void buttonVenda_Click(object sender, EventArgs e)
-        {
-            Vendas vendas = new Vendas(this);
-            vendas.Show();
-        }
+private void buttonCliente_Click(object sender, EventArgs e)
+{
+    Cliente cliente = new Cliente(this, conn);
+    cliente.Show();
+}
 
-        private void buttonProdutos_Click(object sender, EventArgs e)
-        {
-            Produtos produtos = new Produtos(this);
-            produtos.Show();
-        }
+private void buttonVenda_Click(object sender, EventArgs e)
+{
+    Vendas vendas = new Vendas(this);
+    vendas.Show();
+}
 
-        private void StockSellHub_FormClosing(object sender, FormClosingEventArgs e)
-        {
-            conn.Close();
-            MessageBox.Show("conexão fechada");
-        }
+private void buttonProdutos_Click(object sender, EventArgs e)
+{
+    Produtos produtos = new Produtos(this);
+    produtos.Show();
+}
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            MySqlCommand cmd = new MySqlCommand("using StockSellMaster;" +
-                "create table clientes", conn);
-            try
-            {
-                cmd.ExecuteNonQuery();
-                MessageBox.Show("Database criada");
-            }
-            catch
-            {
-                MessageBox.Show("n");
-            }
-        }
+private void StockSellHub_FormClosing(object sender, FormClosingEventArgs e)
+{
+    conn.Close();
+    MessageBox.Show("conexão fechada");
+}
 
-        
+private void button1_Click(object sender, EventArgs e)
+{
+
+    MySqlCommand cmd = new MySqlCommand("create table clientes(" +
+                                            "CPF varchar(11) not null unique," +
+                                            "NomeCompleto text," +
+                                            "DataNascimento date," +
+                                            "primary key(CPF)" +
+                                            ");", conn);
+    try
+    {
+        cmd.ExecuteNonQuery();
+        MessageBox.Show("Database criada");
     }
+    catch(ArgumentException ex)
+    {
+        MessageBox.Show(ex.ToString());
+    }
+}
+
+
+}
 }
